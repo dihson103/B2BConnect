279f7574f471770efbea5e2dbee63a29a936a6ba
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2024-06-27 05:45:56 +0700

    fix login ui

--- web-client/src/app/login/page.tsx
+++ web-client/src/app/login/page.tsx
@@ -1,43 +1,42 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 export default function LoginForm() {
   return (
-    <Card className='mx-auto max-w-sm'>
-      <CardHeader>
-        <CardTitle className='text-2xl'>Đăng nhập</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className='grid gap-4'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input id='email' type='email' placeholder='m@example.com' required />
-          </div>
-          <div className='grid gap-2'>
-            <div className='flex items-center'>
-              <Label htmlFor='password'>Mật khẩu</Label>
-              <Link href='#' className='ml-auto inline-block text-sm underline'>
-                Quên mật khẩu?
-              </Link>
+    <div className='flex min-h-screen items-center justify-center'>
+      <Card className='w-full max-w-sm'>
+        <CardHeader>
+          <CardTitle className='text-2xl'>Đăng nhập</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='grid gap-4'>
+            <div className='grid gap-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input id='email' type='email' placeholder='m@example.com' required />
             </div>
-            <Input id='password' type='password' required />
+            <div className='grid gap-2'>
+              <div className='flex items-center'>
+                <Label htmlFor='password'>Mật khẩu</Label>
+              </div>
+              <Input id='password' type='password' required />
+            </div>
+            <Button type='submit' className='w-full'>
+              Đăng nhập
+            </Button>
+            <Button variant='outline' className='w-full'>
+              Đăng nhập bằng Google
+            </Button>
           </div>
-          <Button type='submit' className='w-full'>
-            Đăng nhập
-          </Button>
-          <Button variant='outline' className='w-full'>
-            Đăng nhập bằng Google
-          </Button>
-        </div>
-        <div className='mt-4 text-center text-sm'>
-          Don&apos;t Đã có tài khoản?{' '}
-          <Link href='#' className='underline'>
-            Đăng ký
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+          <div className='mt-4 text-center text-sm'>
+            Chưa có tài khoản?{' '}
+            <Link href='#' className='underline'>
+              Đăng ký
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
