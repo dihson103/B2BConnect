399c92f68f0730a6fcc4ea51d7baddbfaf44f011
Hennessy Nguyen <anguyen137@fordham.edu>
2024-07-09 09:39:37 +0700

    create register page with rules

---
+++ web-client/src/rules/account.rules.ts
@@ -0,0 +1,14 @@
+import * as z from 'zod'; 
+
+export const CreateAccountSchema = z
+    .object({
+        email: z.string().min(1, {message: "Email không được để trống."}), 
+        password: z.string().min(1, {message: "Password không được để trống"}),
+        retypedPassword: z.string().min(1, {message: "Password nhập lại không được để trống"}),
+    })
+    .refine(data => data.password === data.retypedPassword, {
+        message: "Mật khẩu nhập lại không khớp.", 
+        path: ["retypedPassword"]
+    });
+
+export type CreateAccountFormType = z.infer<typeof CreateAccountSchema>;
