2f9cae7f63a30f26be3f3ce645f4863a930eff91
Hennessy Nguyen <anguyen137@fordham.edu>
2024-07-09 14:29:12 +0700

    add a new testcase

--- Web-backend/test/Application.UnitTests/Queries/Accounts/LoginAccountCommandHandlerTest.cs
+++ Web-backend/test/Application.UnitTests/Queries/Accounts/LoginAccountCommandHandlerTest.cs
@@ -14,14 +14,18 @@ public class LoginAccountCommandHandlerTest
     private readonly Mock<IAccountRepository> _accountRepositoryMock;
     private readonly Mock<IPasswordService> _passwordServiceMock;
     private readonly Mock<IJwtService> _jwtServiceMock;
+    private readonly Mock<IRedisService> _redisServiceMock;
+
     public LoginAccountCommandHandlerTest()
     {
         _accountRepositoryMock = new Mock<IAccountRepository>();
         _passwordServiceMock = new Mock<IPasswordService>();
         _jwtServiceMock = new Mock<IJwtService>();
+        _redisServiceMock = new Mock<IRedisService>();
         _handler = new LoginAccountCommandHandler(
             _accountRepositoryMock.Object, 
             _passwordServiceMock.Object, 
+            _redisServiceMock.Object,
             _jwtServiceMock.Object);
     }
 
@@ -41,5 +45,16 @@ public class LoginAccountCommandHandlerTest
         await Assert.ThrowsAsync<MyUnauthorizedException>(async () => {
            var result = await _handler.Handle(loginCommand, default);
         });
+    }
+
+    [Fact]
+    public async Task Handle_ShouldLogIn_WhenSuccess() {
+        var loginCommand = new LoginCommand("email", "password");
+        _accountRepositoryMock.Setup(s => s.LoginAsync(It.IsAny<string>()))
+            .ReturnsAsync(Account.Create("email", "hashpassword"));
+        _passwordServiceMock.Setup(s => s.IsVerify(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+        var result = await _handler.Handle(loginCommand, default);
+        Assert.NotNull(result);
+        Assert.Equal(loginCommand.Email, result.Data.account.Email);
     }
 }
