3fd7aba1b10e61d12150f2fe9e43f32175af7349
NguyenDinhTruong <truongx62001@gmail.com>
2024-07-22 13:26:35 +0700

    update search without Diacritic

--- Web-backend/src/Application/Utils/StringHandlerUtil.cs
+++ Web-backend/src/Application/Utils/StringHandlerUtil.cs
@@ -1,33 +1,43 @@
 ﻿
-using System.Globalization;
-using System.Text;
-
 namespace Application.Utils;
 public class StringHandlerUtil
 {
 
     public static string RemoveDiacritics(string text)
     {
+        if (string.IsNullOrEmpty(text))
+            return text;
+
         text = text.Trim();
-        string[] arr1 = new string[] { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ",
-    "đ",
-    "é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ",
-    "í","ì","ỉ","ĩ","ị",
-    "ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ",
-    "ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự",
-    "ý","ỳ","ỷ","ỹ","ỵ",};
-        string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
-    "d",
-    "e","e","e","e","e","e","e","e","e","e","e",
-    "i","i","i","i","i",
-    "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",
-    "u","u","u","u","u","u","u","u","u","u","u",
-    "y","y","y","y","y",};
+
+        char[] arr1 = new char[]
+        {
+        'á', 'à', 'ả', 'ã', 'ạ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ',
+        'đ',
+        'é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ế', 'ề', 'ể', 'ễ', 'ệ',
+        'í', 'ì', 'ỉ', 'ĩ', 'ị',
+        'ó', 'ò', 'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ',
+        'ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự',
+        'ý', 'ỳ', 'ỷ', 'ỹ', 'ỵ'
+        };
+
+        char[] arr2 = new char[]
+        {
+        'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+        'd',
+        'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e',
+        'i', 'i', 'i', 'i', 'i',
+        'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
+        'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
+        'y', 'y', 'y', 'y', 'y'
+        };
+
         for (int i = 0; i < arr1.Length; i++)
         {
             text = text.Replace(arr1[i], arr2[i]);
-            text = text.Replace(arr1[i].ToUpper(), arr2[i].ToUpper());
+            text = text.Replace(char.ToLower(arr1[i]), char.ToLower(arr2[i]));
         }
+
         return text;
     }
 }
--- Web-backend/src/Contract/Services/Business/Share/BusinessResponse.cs
+++ Web-backend/src/Contract/Services/Business/Share/BusinessResponse.cs
@@ -5,5 +5,5 @@ using Contract.Services.Representative.Share;
 namespace Contract.Services.Business.Share;
 public record BusinessResponse(Guid Id, string TaxCode, string Name, 
     DateOnly DateOfEstablishment, string? WebSite, string? AvatarImage
-    , string? CoverImage, NumberOfEmployee NumberOfEmployee, bool IsVerified, AccountResponse AccountResponse,
+    , string? CoverImage, string NumberOfEmployee, bool IsVerified, AccountResponse AccountResponse,
     RepresentativeResponse RepresentativeResponse, List<BranchResponse> BranchResponses);
